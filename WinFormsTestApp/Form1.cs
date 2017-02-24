75b9573c58ce047161132ae6a4dd5ce70611de06
Andy De George <adegeo@microsoft.com>
2017-02-24 15:17:12 -0800

    Added test app

---
+++ WinFormsTestApp/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.DoubleBuffered = true;
+            fontMaster = SadConsole.FontMaster.LoadFont("IBM.font");
+            console = new SadConsole.Consoles.SurfaceEditor(new SadConsole.Consoles.TextSurface(80, 100));
+            renderer = new SadConsole.Consoles.TextSurfaceRenderer();
+        }
+
+        int rotation = 0;
+        byte color = 0;
+        Color foreground = Color.Purple;
+        SadConsole.FontMaster fontMaster;
+        SadConsole.Consoles.SurfaceEditor console;
+        SadConsole.Consoles.TextSurfaceRenderer renderer;
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            System.Drawing.Graphics graphics = e.Graphics;
+
+            SadConsole.FontMaster.LoadFont("IBM.font"); // Sets default font if not set
+
+            var textSurface = new SadConsole.Consoles.TextSurface(80, 24);
+            var console = new SadConsole.Consoles.SurfaceEditor(textSurface);
+            var renderer = new SadConsole.Consoles.TextSurfaceRenderer();
+
+            var doc = new SadConsole.Ansi.Document("QS-SIERR.ANS");
+            var writer = new SadConsole.Ansi.AnsiWriter(doc, console);
+            writer.ReadEntireDocument();
+
+            // Get times scrolled
+            if (console.TimesShiftedUp != 0)
+            {
+                textSurface.Dispose();
+                console.TextSurface = textSurface = new SadConsole.Consoles.TextSurface(80, 24 + console.TimesShiftedUp);
+
+                console.ClearShiftValues();
+
+                writer = new SadConsole.Ansi.AnsiWriter(doc, console);
+                writer.ReadEntireDocument();
+            }
+
+            // Create graphics image
+            //Bitmap outputImage = new Bitmap(console.TextSurface.Width * SadConsole.FontMaster.DefaultFont.Size.X,
+            //                                 console.TextSurface.Height * SadConsole.FontMaster.DefaultFont.Size.Y);
+            //Graphics graphics = Graphics.FromImage(outputImage);
+            graphics.Clear(Color.Black);
+
+            // Draw ansi surface to image
+            renderer.RenderTarget = graphics;
+            renderer.Render((SadConsole.Consoles.TextSurface)console.TextSurface, new Point(0, 0), false);
+
+            // Save image
+            //outputImage.Save("ansi.png", ImageFormat.Png);
+
+            // Clean up
+            //graphics.Dispose();
+            //outputImage.Dispose();
+            textSurface.Dispose();
+        }
+    }
+}
---
+++ WinFormsTestApp/Program.old.cs
@@ -0,0 +1,186 @@
+﻿///*
+//using SadConsole.Consoles;
+//using System;
+//using SadConsole;
+//using Microsoft.Xna.Framework;
+//using SadConsole.Input;
+//*/
+
+//using System.Drawing;
+
+//namespace StarterProject
+//{
+//    class Program
+//    {
+//        //private static Windows.CharacterViewer _characterWindow;
+//        //private static int currentConsoleIndex;
+
+
+
+//        static void Main(string[] args)
+//        {
+//            //var a = SharpDX.Direct2D1.SolidColorBrush;
+//            //a.Color
+
+//            //// Setup the engine and creat the main window.
+//            //SadConsole.Engine.Initialize("IBM.font", 80, 25);
+
+//            //// Hook the start event so we can add consoles to the system.
+//            //SadConsole.Engine.EngineStart += Engine_EngineStart;
+
+//            //// Hook the update event that happens each frame so we can trap keys and respond.
+//            //SadConsole.Engine.EngineUpdated += Engine_EngineUpdated;
+
+//            //SadConsole.Engine.EngineDrawFrame += Engine_EngineDrawFrame;
+
+//            //// Start the game.
+//            //SadConsole.Engine.Run();
+
+//            System.Drawing.Bitmap bitmap = new System.Drawing.Bitmap(200, 200);
+//            System.Drawing.Graphics graphics = System.Drawing.Graphics.FromImage(bitmap);
+//            graphics.FillRectangle(System.Drawing.Brushes.Green, new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height));
+//            SadConsole.FontMaster fontMaster = SadConsole.FontMaster.LoadFont("IBM.font");
+//            var font = fontMaster.GetFont(SadConsole.Font.FontSizes.One);
+
+//            System.Drawing.SpriteBatch batch = new System.Drawing.SpriteBatch();
+//            var cell = new SadConsole.Cell() { Background = Color.Yellow, Foreground = Color.Purple, GlyphIndex = 'A' };
+//            batch.Reset(graphics, new System.Drawing.Drawing2D.Matrix());
+//            batch.DrawCell(cell, new Rectangle(0, 0, font.Size.X, font.Size.Y), font.SolidGlyphRectangle, Color.Orange, font);
+//            batch.End();
+
+//            bitmap.Save(@"c:\temp\output.png", System.Drawing.Imaging.ImageFormat.Png);
+
+//            bitmap.Dispose();
+//            graphics.Dispose();
+//            fontMaster.Image.Dispose();
+
+//            //OpenTK.Graphics.
+
+//            //OpenTK.GameWindow win = new OpenTK.GameWindow(200, 200, OpenTK.Graphics.GraphicsMode.Default, "test", OpenTK.GameWindowFlags.Default, OpenTK.DisplayDevice.Default);
+//            //win.Run();
+
+//        }
+
+//        /*
+//        private static void Engine_EngineDrawFrame(object sender, EventArgs e)
+//        {
+//            // Custom drawing. You don't usually have to do this.
+//        }
+
+//        private static void Engine_EngineUpdated(object sender, EventArgs e)
+//        {
+//            if (!_characterWindow.IsVisible)
+//            {
+//                // This block of code cycles through the consoles in the SadConsole.Engine.ConsoleRenderStack, showing only a single console
+//                // at a time. This code is provided to support the custom consoles demo. If you want to enable the demo, uncomment one of the lines
+//                // in the Initialize method above.
+//                if (SadConsole.Engine.Keyboard.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.F1))
+//                {
+//                    MoveNextConsole();
+//                }
+//                else if (SadConsole.Engine.Keyboard.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.F2))
+//                {
+//                    _characterWindow.Show(true);
+//                }
+//                else if (SadConsole.Engine.Keyboard.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.F3))
+//                {
+//                }
+//            }
+//        }
+
+//        private static void Engine_EngineStart(object sender, EventArgs e)
+//        {
+//            // Setup our custom theme.
+//            Theme.SetupThemes();
+
+//            // By default SadConsole adds a blank ready-to-go console to the rendering system. 
+//            // We don't want to use that for the sample project so we'll remove it.
+//            SadConsole.Engine.ConsoleRenderStack.Clear();
+//            SadConsole.Engine.ActiveConsole = null;
+
+//            // We'll instead use our demo consoles that show various features of SadConsole.
+//            SadConsole.Engine.ConsoleRenderStack
+//                = new ConsoleList() {
+//                    new CustomConsoles.SubConsoleCursor(),
+//                                        new CustomConsoles.ControlsTest(),
+//                                        new CustomConsoles.SplashScreen() { SplashCompleted = () => { MoveNextConsole(); } },
+//                                        new CustomConsoles.StretchedConsole(),
+//                                        //new CustomConsoles.CachedConsoleConsole(),
+//                                        new CustomConsoles.StringParsingConsole(),
+//                                        //new CustomConsoles.CursorConsole(),
+//                                        //new CustomConsoles.DOSConsole(),
+//                                        //new CustomConsoles.SceneProjectionConsole(),
+//                                        new CustomConsoles.ScrollableConsole(10, 10, 20),
+//                                        new CustomConsoles.ViewsAndSubViews(),
+//                                        new CustomConsoles.StaticConsole(),
+//                                        new CustomConsoles.BorderedConsole(),
+//                                        new CustomConsoles.GameObjectConsole(),
+//                                        new CustomConsoles.RandomScrollingConsole(),
+//                                        new CustomConsoles.WorldGenerationConsole(),
+//                                    };
+
+//            // Show the first console (by default all of our demo consoles are hidden)
+//            SadConsole.Engine.ConsoleRenderStack[0].IsVisible = true;
+
+//            // Set the first console in the console list as the "active" console. This allows the keyboard to be processed on the console.
+//            SadConsole.Engine.ActiveConsole = SadConsole.Engine.ConsoleRenderStack[0];
+
+//            // Initialize the windows
+//            _characterWindow = new Windows.CharacterViewer();
+
+//            //SadConsole.Effects.Fade a = new SadConsole.Effects.Fade();
+//            //a.DestinationForeground = Microsoft.Xna.Framework.Color.Turquoise;
+//            //SadConsole.Engine.MonoGameInstance.Components.Add(new FPSCounterComponent(SadConsole.Engine.MonoGameInstance));
+
+//        }
+
+//        private static void MoveNextConsole()
+//        {
+//            currentConsoleIndex++;
+
+//            if (currentConsoleIndex >= SadConsole.Engine.ConsoleRenderStack.Count)
+//                currentConsoleIndex = 0;
+
+//            for (int i = 0; i < SadConsole.Engine.ConsoleRenderStack.Count; i++)
+//                SadConsole.Engine.ConsoleRenderStack[i].IsVisible = currentConsoleIndex == i;
+
+//            Engine.ActiveConsole = SadConsole.Engine.ConsoleRenderStack[currentConsoleIndex];
+//        }
+//        */
+//    }
+//}
+
+////using System.Drawing;
+////using System.Drawing.Imaging;
+////using OpenTK.Graphics;
+////using OpenTK.Graphics.OpenGL;
+
+////using SDPixelFormat = System.Drawing.Imaging.PixelFormat;
+////using PixelFormat = OpenTK.Graphics.OpenGL.PixelFormat;
+
+////namespace StarterProject
+////{
+////    static class Program
+////    {
+////        static void Main(string[] args)
+////        {
+////            var mode = new GraphicsMode(new ColorFormat(8, 8, 8, 8), 24, 0, 0, ColorFormat.Empty, 1);
+////            var win = new OpenTK.GameWindow(640, 480, mode, "", OpenTK.GameWindowFlags.Default, OpenTK.DisplayDevice.Default, 3, 0, GraphicsContextFlags.Default);
+////            win.Visible = false;
+////            win.MakeCurrent();
+////            /* START OF YOUR ACTUAL GL CODE */
+////            GL.ClearColor(0.7f, 0.7f, 1.0f, 1.0f);
+////            GL.Clear(ClearBufferMask.ColorBufferBit);
+////            /* END OF YOUR ACTUAL GL CODE */
+////            GL.Flush();
+////            using (var bmp = new Bitmap(640, 480, SDPixelFormat.Format32bppArgb))
+////            {
+////                var mem = bmp.LockBits(new Rectangle(0, 0, 640, 480), ImageLockMode.WriteOnly, SDPixelFormat.Format32bppArgb);
+////                GL.PixelStore(PixelStoreParameter.PackRowLength, mem.Stride / 4);
+////                GL.ReadPixels(0, 0, 640, 480, PixelFormat.Bgra, PixelType.UnsignedByte, mem.Scan0);
+////                bmp.UnlockBits(mem);
+////                bmp.Save(@"D:\test.png", ImageFormat.Png);
+////            }
+////        }
+////    }
+////}
